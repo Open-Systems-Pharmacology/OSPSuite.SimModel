f111ff46d0ade6c1b62f04ebb19748ad4769fc33
Yuri05 <Yuri05@github.com>
2019-07-29 19:30:41 +0200

    WIP

---
+++ src/OSPSuite.SimModel/Simulation.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OSPSuite.SimModel
+{
+   [StructLayout(LayoutKind.Sequential)]
+   internal struct SimulationOptionsStructure
+   {
+      public bool ShowProgress;
+      public double ExecutionTimeLimit;
+      public bool StopOnWarnings;
+      public bool AutoReduceTolerances;
+      public bool WriteLogFile;
+      public bool CheckForNegativeValues;
+      public bool ValidateWithXMLSchema;
+      public bool IdentifyUsedParameters;
+      public bool KeepXMLNodeAsString;
+      public bool UseFloatComparisonInUserOutputTimePoints;
+   }
+
+   internal class SimulationImports
+   {
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern IntPtr CreateSimulation();
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void DisposeSimulation(IntPtr simulation);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void LoadSimulationFromXMLFile(IntPtr simulation, string fileName, out bool success, out string errorMessage);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void LoadSimulationFromXMLString(IntPtr simulation, string simulationXML, out bool success, out string errorMessage);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void FinalizeSimulation(IntPtr simulation, out bool success, out string errorMessage);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern int GetSimulationProgress(IntPtr simulation);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void CancelSimulationRun(IntPtr simulation);
+
+      //TODO
+      //char* GetSimModelVersion();
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern string GetObjectPathDelimiter(IntPtr simulation);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern SimulationOptionsStructure GetSimulationOptions(IntPtr simulation);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void SetSimulationOptions(IntPtr simulation, SimulationOptionsStructure options);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void RunSimulation(IntPtr simulation, out bool toleranceWasReduced, out double newAbsTol, out double newRelTol, out bool success, out string errorMessage);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern int GetNumberOfTimePoints(IntPtr simulation);
+
+      //The caller must call GetNumberOfTimePoints() first and pass an array where NumberOfTimePoints elements are preallocated 
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void FillTimeValues(IntPtr simulation, [In, Out] double[] timepoints, out bool success, out string errorMessage);
+
+      //returns XML string of the simulation (with updated formulas for not fixed parameters/species initial values)
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern string GetSimulationXMLString(IntPtr simulation, out bool success, out string errorMessage);
+
+      //checks if simulation has any contains parameters
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern bool ContainsPersistableParameters(IntPtr simulation, out bool success, out string errorMessage);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern int GetXMLVersion(IntPtr simulation);
+
+      [DllImport(SimModelImportDefinitions.NATIVE_DLL, CallingConvention = SimModelImportDefinitions.CALLING_CONVENTION)]
+      public static extern void ReleaseSimulationMemory(IntPtr simulation, out bool success, out string errorMessage);
+   }
+   public class Simulation : IDisposable
+   {
+      private readonly IntPtr _simulation;
+      private SimulationOptionsStructure _simulationOptions;
+
+      private bool _toleranceWasReduced;
+      private double _newAbsTol, _newRelTol;
+
+      private bool _disposed = false;
+
+      private void evaluateCppCallResult(bool success, string errorMessage)
+      {
+         if (success)
+            return;
+
+         //TODO
+         throw new Exception(errorMessage);
+      }
+
+      private void setOptions(Action action)
+      {
+         action();
+         SimulationImports.SetSimulationOptions(_simulation, _simulationOptions);
+      }
+
+      public Simulation()
+      {
+         _simulation = SimulationImports.CreateSimulation();
+         _simulationOptions = SimulationImports.GetSimulationOptions(_simulation);
+
+         _toleranceWasReduced = false;
+         _newAbsTol = double.NaN;
+         _newRelTol = double.NaN;
+      }
+
+      public void LoadFromXMLFile(string fileName)
+      {
+         SimulationImports.LoadSimulationFromXMLFile(_simulation, fileName, out var success, out var errorMessage);
+         evaluateCppCallResult(success, errorMessage);
+      }
+
+      public void LoadFromXMLString(string xmlString)
+      {
+         SimulationImports.LoadSimulationFromXMLString(_simulation, xmlString, out var success, out var errorMessage);
+         evaluateCppCallResult(success, errorMessage);
+      }
+
+      public void FinalizeSimulation()
+      {
+         SimulationImports.FinalizeSimulation(_simulation, out var success, out var errorMessage);
+         evaluateCppCallResult(success, errorMessage);
+      }
+
+      public int Progress => SimulationImports.GetSimulationProgress(_simulation);
+
+      public void Cancel()
+      {
+         SimulationImports.CancelSimulationRun(_simulation);
+      }
+
+      //TODO
+      //char* GetSimModelVersion();
+
+      public string ObjectPathDelimiter => SimulationImports.GetObjectPathDelimiter(_simulation);
+
+      public bool ShowProgress
+      {
+         get => _simulationOptions.ShowProgress;
+         set => setOptions(() => _simulationOptions.ShowProgress = value);
+      }
+
+      public double ExecutionTimeLimit
+      {
+         get => _simulationOptions.ExecutionTimeLimit;
+         set => setOptions(()=>_simulationOptions.ExecutionTimeLimit = value); 
+      }
+
+      public bool StopOnWarnings
+      {
+         get => _simulationOptions.StopOnWarnings;
+         set => setOptions(() => _simulationOptions.StopOnWarnings = value);
+      }
+
+      /// <summary>
+      /// If solving of the diff. eq. systems fails with a convergence error <para></para>
+      ///AND the value of AutoReduceTolerances is TRUE, both absolute and relative <para></para>
+      ///tolerance will be reduced by 10, until either the system was successfully solved <para></para>
+      ///or the predefined lower bounds for both tolerances were reached
+      ///
+      ///Default is TRUE
+      /// </summary>
+      public bool AutoReduceTolerances
+      {
+         get => _simulationOptions.AutoReduceTolerances;
+         set => setOptions(() => _simulationOptions.AutoReduceTolerances = value);
+      }
+
+      public bool WriteLogFile
+      {
+         get => _simulationOptions.WriteLogFile;
+         set => setOptions(() => _simulationOptions.WriteLogFile = value);
+      }
+
+      public bool CheckForNegativeValues
+      {
+         get => _simulationOptions.CheckForNegativeValues;
+         set => setOptions(() => _simulationOptions.CheckForNegativeValues = value);
+      }
+
+      public bool ValidateWithXMLSchema
+      {
+         get => _simulationOptions.ValidateWithXMLSchema;
+         set => setOptions(() => _simulationOptions.ValidateWithXMLSchema = value);
+      }
+
+      public bool IdentifyUsedParameters
+      {
+         get => _simulationOptions.IdentifyUsedParameters;
+         set => setOptions(() => _simulationOptions.IdentifyUsedParameters = value);
+      }
+
+      public bool KeepXMLNodeAsString
+      {
+         get => _simulationOptions.KeepXMLNodeAsString;
+         set => setOptions(() => _simulationOptions.KeepXMLNodeAsString = value);
+      }
+
+      public bool UseFloatComparisonInUserOutputTimePoints
+      {
+         get => _simulationOptions.UseFloatComparisonInUserOutputTimePoints;
+         set => setOptions(() => _simulationOptions.UseFloatComparisonInUserOutputTimePoints = value);
+      }
+
+      public void RunSimulation()
+      {
+         SimulationImports.RunSimulation(_simulation, out _toleranceWasReduced,out _newAbsTol,out _newRelTol, out var success, out var errorMessage);
+         evaluateCppCallResult(success, errorMessage);
+      }
+
+      public bool ToleranceWasReduced => _toleranceWasReduced;
+
+      public double UsedAbsoluteTolerance => _newAbsTol;
+
+      public double UsedRelativeTolerance => _newRelTol;
+
+      public int GetNumberOfTimePoints => SimulationImports.GetNumberOfTimePoints(_simulation);
+      
+      public double[] SimulationTimes
+      {
+         get
+         {
+            var simulationTimes = new double[GetNumberOfTimePoints];
+
+            SimulationImports.FillTimeValues(_simulation, simulationTimes, out var success, out var errorMessage);
+            evaluateCppCallResult(success, errorMessage);
+
+            return simulationTimes;
+         }
+      }
+
+      public string SimulationXMLString
+      {
+         get
+         {
+            var xmlString = SimulationImports.GetSimulationXMLString(_simulation, out var success, out var errorMessage);
+            evaluateCppCallResult(success, errorMessage);
+
+            return xmlString;
+         }
+      }
+
+      public bool ContainsPersistableParameters
+      {
+         get
+         {
+            var containsPersistable = SimulationImports.ContainsPersistableParameters(_simulation, out var success, out var errorMessage);
+            evaluateCppCallResult(success, errorMessage);
+
+            return containsPersistable;
+         }
+      }
+
+      public int XMLVersion => SimulationImports.GetXMLVersion(_simulation);
+
+      public void ReleaseMemory()
+      {
+         SimulationImports.ReleaseSimulationMemory(_simulation, out var success, out var errorMessage);
+         evaluateCppCallResult(success, errorMessage);
+      }
+      //-------------------------------------------------------------------------------------------------
+
+      //-------------------------------------------------------------------------------------------------
+
+      public void Dispose()
+      {
+         Dispose(true);
+         GC.SuppressFinalize(this);
+      }
+
+      // Protected implementation of Dispose pattern.
+      protected virtual void Dispose(bool disposing)
+      {
+         if (_disposed)
+            return;
+
+         if (disposing)
+         {
+            SimulationImports.DisposeSimulation(_simulation);
+         }
+
+         _disposed = true;
+      }
+   }
+}
---
+++ src/OSPSuite.SimModelNative/include/SimModel/PInvokeSimulation.h
@@ -0,0 +1,96 @@
+#ifndef _PInvokeSimulation_H_
+#define _PInvokeSimulation_H_
+
+#include "SimModel/Simulation.h"
+#include "SimModel/SimulationOptions.h"
+
+namespace SimModelNative
+{
+   struct SimulationOptionsStructure
+   {
+      bool ShowProgress;
+		double ExecutionTimeLimit;
+		bool StopOnWarnings;
+		bool AutoReduceTolerances;
+		bool WriteLogFile;
+		bool CheckForNegativeValues;
+      bool ValidateWithXMLSchema;
+      bool IdentifyUsedParameters;
+      bool KeepXMLNodeAsString;
+		bool UseFloatComparisonInUserOutputTimePoints;
+
+      void CopyFrom(const SimulationOptions& options);
+   };
+
+   //-------------- C interface for PInvoke -----------------------------------------
+   extern "C"
+   {
+      SIM_EXPORT Simulation* CreateSimulation();
+      SIM_EXPORT void DisposeSimulation(Simulation* simulation);
+
+      SIM_EXPORT void LoadSimulationFromXMLFile(Simulation* simulation, const char* fileName, bool& success, char** errorMessage);
+      SIM_EXPORT void LoadSimulationFromXMLString(Simulation* simulation, const char* simulationXML, bool& success, char** errorMessage);
+      SIM_EXPORT void FinalizeSimulation(Simulation* simulation, bool& success, char** errorMessage);
+      SIM_EXPORT long GetSimulationProgress(Simulation* simulation);
+      SIM_EXPORT void CancelSimulationRun(Simulation* simulation);
+      //SIM_EXPORT char* GetSimModelVersion();
+      SIM_EXPORT char* GetObjectPathDelimiter(Simulation* simulation);
+
+      SIM_EXPORT SimulationOptionsStructure GetSimulationOptions(Simulation* simulation);
+      SIM_EXPORT void SetSimulationOptions(Simulation* simulation, SimulationOptionsStructure options);
+
+      SIM_EXPORT void RunSimulation(Simulation* simulation, bool& toleranceWasReduced, double& newAbsTol, double& newRelTol, bool& success, char** errorMessage);
+
+      SIM_EXPORT int GetNumberOfTimePoints(Simulation* simulation);
+
+      //The caller must call GetNumberOfTimePoints() first and pass an array where NumberOfTimePoints elements are preallocated 
+      SIM_EXPORT void FillTimeValues(Simulation* simulation, double* timepoints, bool& success, char** errorMessage);
+
+      //returns XML string of the simulation (with updated formulas for not fixed parameters/species initial values)
+      SIM_EXPORT char* GetSimulationXMLString(Simulation* simulation, bool& success, char** errorMessage);
+
+      //checks if simulation has any contains parameters
+      SIM_EXPORT bool ContainsPersistableParameters(Simulation* simulation, bool& success, char** errorMessage);
+
+      SIM_EXPORT int GetXMLVersion(Simulation* simulation);
+
+      SIM_EXPORT void ReleaseSimulationMemory(Simulation* simulation, bool& success, char** errorMessage);
+
+      // -----------------------------------------------------------------------------------------------------------------
+
+      //SIM_EXPORT TObjectList<Species>& SpeciesList();
+      //SIM_EXPORT TObjectList<Observer>& Observers();
+
+      //SIM_EXPORT const TObjectVector<SolverWarning>& SolverWarnings() const;
+      //SIM_EXPORT OutputSchema& GetOutputSchema();
+
+      ////fill the properties of all simulation observers
+      //SIM_EXPORT void FillObserverProperties(std::vector<QuantityInfo>& observerProperties);
+
+      ////fill the properties of all simulation parameters
+      //SIM_EXPORT void FillParameterProperties(std::vector<ParameterInfo>& paramProperties);
+
+      ////set variable parameters
+      //SIM_EXPORT void SetVariableParameters(std::vector<ParameterInfo>& paramProperties);
+
+      ////fill the properties of all simulation DE variables
+      //SIM_EXPORT void FillDEVariableProperties(std::vector<SpeciesInfo>& variableProperties);
+
+      ////set variable DE Variables
+      //SIM_EXPORT void SetVariableDEVariables(std::vector<SpeciesInfo>& variableProperties);
+
+      ////sets parameter values into the simulation for the next run
+      //SIM_EXPORT void SetParametersValues(std::vector<ParameterInfo>& paramProperties);
+
+      ////sets DE variable properties into the simulation for the next run
+      //SIM_EXPORT void SetDEVariablesProperties(std::vector<SpeciesInfo>& variableProperties);
+
+      //SIM_EXPORT bool UseBandLinearSolver();
+      //SIM_EXPORT void SetUseBandLinearSolver(bool useBandLinearSolver);
+   }
+
+}//.. end "namespace SimModelNative"
+
+
+#endif //_PInvokeSimulation_H_
+
