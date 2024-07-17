ab4dbab7af75f51e27a671aeefeeecbba21678c3
benjaperez1983 <benjamin.perez@gmail.com>
2024-07-17 14:30:08 +0200

    Change dll name

--- src/OSPSuite.SimModel/SimModelImportDefinitions.cs
+++ src/OSPSuite.SimModel/SimModelImportDefinitions.cs
@@ -5,12 +5,7 @@ namespace OSPSuite.SimModel
 {
    internal class SimModelImportDefinitions
    {
-
-#if _WINDOWS
-      public const String NATIVE_DLL = "OSPSuite.SimModelNative.dll";
-#else
-      public const String NATIVE_DLL = "libOSPSuite.SimModelNative.so";
-#endif
+      public const String NATIVE_DLL = "OSPSuite.SimModelNative";
 
       public const CallingConvention CALLING_CONVENTION = CallingConvention.Cdecl;
    }
