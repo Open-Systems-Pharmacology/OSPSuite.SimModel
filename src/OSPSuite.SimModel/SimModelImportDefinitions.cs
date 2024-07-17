2ad736ae3ae8b12422ad9de77ddb5d8d4d3ecff1
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-07-17 12:19:59 -0400

    multi-platform build

--- src/OSPSuite.SimModel/SimModelImportDefinitions.cs
+++ src/OSPSuite.SimModel/SimModelImportDefinitions.cs
@@ -5,7 +5,12 @@ namespace OSPSuite.SimModel
 {
    internal class SimModelImportDefinitions
    {
+
+#if NETSTANDARD2_0
       public const String NATIVE_DLL = "OSPSuite.SimModelNative";
+#else
+      public const String NATIVE_DLL = "OSPSuite.SimModelNative.dll";
+#endif
 
       public const CallingConvention CALLING_CONVENTION = CallingConvention.Cdecl;
    }
