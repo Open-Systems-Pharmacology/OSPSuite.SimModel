071b32c02cfe080158b42fc6b69363d83d2c7257
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-12-19 11:36:35 -0500

    update build file

--- buildNix.sh
+++ buildNix.sh
@@ -5,12 +5,14 @@
 
 rm -f OSPSuite.SimModel4Nix.sln
 
-if [ $1="MacOS" ]
+if [ "$1" = "MacOS" ];
 then
     EXT="dylib"
 else
     EXT="so"
 fi
+
+echo $EXT
 
 git submodule update --init --recursive
 nuget sources add -name bddhelper -source https://ci.appveyor.com/nuget/ospsuite-bddhelper 
@@ -31,13 +33,13 @@ dotnet sln OSPSuite.SimModel4Nix.sln remove src/OSPSuite.SysTool/OSPSuite.SysToo
 dotnet sln OSPSuite.SimModel4Nix.sln remove src/OSPSuite.XMLWrapper/OSPSuite.XMLWrapper.vcxproj
 dotnet sln OSPSuite.SimModel4Nix.sln remove tests/TestAppNetCore/TestAppNetCore.csproj
 
-cmake -BBuild/Release/x64/ -Hsrc/OSPSuite.SimModelNative/ -DCMAKE_BUILD_TYPE=Release -DFuncParserDir=Build/Release/x64/ -DEXT=$EXT -DCMAKE_OSX_ARCHITECTURES=x86_64
+cmake -BBuild/Release/x64/ -Hsrc/OSPSuite.SimModelNative/ -DCMAKE_BUILD_TYPE=Release -DFuncParserDir=Build/Release/x64/ -DEXT=$EXT
 cp packages/OSPSuite.FuncParser.$1/OSPSuite.FuncParserNative/bin/native/x64/Release/libOSPSuite.FuncParserNative.$EXT Build/Release/x64/
 cp packages/OSPSuite.SimModelSolver_CVODES.$1/OSPSuite.SimModelSolver_CVODES/bin/native/x64/Release/libOSPSuite.SimModelSolver_CVODES.$EXT Build/Release/x64/
 make -C Build/Release/x64/
 dotnet build OSPSuite.SimModel4Nix.sln /property:Configuration=Release;Platform=x64
 
-cmake -BBuild/Debug/x64/ -Hsrc/OSPSuite.SimModelNative/ -DCMAKE_BUILD_TYPE=Debug -DFuncParserDir=Build/Debug/x64/ -DEXT=$EXT -DCMAKE_OSX_ARCHITECTURES=x86_64
+cmake -BBuild/Debug/x64/ -Hsrc/OSPSuite.SimModelNative/ -DCMAKE_BUILD_TYPE=Debug -DFuncParserDir=Build/Debug/x64/ -DEXT=$EXT 
 cp packages/OSPSuite.FuncParser.$1/OSPSuite.FuncParserNative/bin/native/x64/Debug/libOSPSuite.FuncParserNative.$EXT Build/Debug/x64/
 cp packages/OSPSuite.SimModelSolver_CVODES.$1/OSPSuite.SimModelSolver_CVODES/bin/native/x64/Debug/libOSPSuite.SimModelSolver_CVODES.$EXT Build/Debug/x64/
 make -C Build/Debug/x64/
@@ -47,6 +49,6 @@ dotnet build OSPSuite.SimModel4Nix.sln /property:Configuration=Debug;Platform=x6
 export LD_LIBRARY_PATH=Build/Release/x64:$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=./Build/Release/x64:$DYLD_LIBRARY_PATH
 
-/usr/local/share/dotnet/x64/dotnet test tests/OSPSuite.SimModel.Tests/OSPSuite.SimModel.Tests.csproj --no-build --no-restore --configuration:Release --verbosity detailed
+dotnet test tests/OSPSuite.SimModel.Tests/OSPSuite.SimModel.Tests.csproj --no-build --no-restore --configuration:Release --verbosity normal 
 
 dotnet pack src/OSPSuite.SimModel/ -p:PackageVersion=$2
